2e643a47f172136701cdbd61008c55bae3624b39
Lucas Nguyen <lucas.nguyen123@gmail.com>
2017-02-18 19:06:16 +0800

    1. Did Scene Manager

--- DM2122_Framework/Application/Source/Application.cpp
+++ DM2122_Framework/Application/Source/Application.cpp
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "SceneManager.h"
 #include "StudioProject.h"
 #include "SPTest.h"
 #include "MainMenu.h"
@@ -104,40 +105,19 @@ void Application::Init()
 void Application::Run()
 {
 	//Main Loop
-//	Scene *scene1 = new SceneText();
-//	Scene *scene2 = new SceneUI();
-//	Scene *scene = scene1;
-	Scene *scene = new SPTest();
-	scene->Init();
+	SceneManager::getInstance()->AddScene(new StudioProject()); // Main Carnival Scene is scene 1
+	SceneManager::getInstance()->AddScene(new SPTest()); // SPTest Scene is scene 2
 
 	m_timer.startTimer();    // Start timer to calculate how long it takes to render this frame
 	while (!glfwWindowShouldClose(m_window) && !IsKeyPressed(VK_ESCAPE))
 	{
-	/*	if (IsKeyPressed(VK_F1) && scene != scene1)
-		{
-			scene->Exit();
-			scene = scene1;
-			scene->Init();
-		}
-		else if (IsKeyPressed(VK_F2) && (scene != scene2))
-		{
-			scene->Exit();
-			scene = scene2;
-			scene->Init();
-		}*/
-
-		scene->Update(m_timer.getElapsedTime());
-		scene->Render();
+		SceneManager::getInstance()->Update(m_timer.getElapsedTime());
 		//Swap buffers
 		glfwSwapBuffers(m_window);
 		//Get and organize events, like keyboard and mouse input, window resizing, etc...
 		glfwPollEvents();
         m_timer.waitUntil(frameTime);       // Frame rate limiter. Limits each frame to a specified time in ms.   
 	} //Check if the ESC key had been pressed or if the window had been closed
-	scene->Exit();
-//	delete scene1;
-//	delete scene2;
-
 }
 
 void Application::Exit()
---
+++ DM2122_Framework/Application/Source/SceneManager.cpp
@@ -0,0 +1,63 @@
+#include "Application.h"
+#include "SceneManager.h"
+#include "MainMenu.h"
+
+SceneManager *SceneManager::sceneManager = NULL;
+
+SceneManager::~SceneManager()
+{
+	currSceneID = 0;
+	while (maps[currSceneID] != NULL)
+	{
+		delete maps[currSceneID];
+		currSceneID++;
+	}
+	delete sceneManager;
+}
+
+ SceneManager *SceneManager::getInstance()
+{
+	if (sceneManager == NULL)
+	{
+		sceneManager = new SceneManager();
+		sceneManager->AddScene(new MainMenu());
+		sceneManager->maps[0]->Init();
+	}
+	return sceneManager;
+}
+
+void SceneManager::AddScene(Scene *scene)
+{
+	maps[maps.size()] = scene;
+}
+void SceneManager::SetNextScene(int sceneID)
+{
+	nextSceneID = sceneID;
+}
+
+void SceneManager::Update(float dt)
+{
+	if (currSceneID == 0)
+	{
+		if (maps[currSceneID]->changeScene == 1)
+		{
+			maps[currSceneID]->Exit();
+			SetNextScene(1);
+			currSceneID = nextSceneID;
+			maps[currSceneID]->Init();
+		}
+	}
+	else if (currSceneID == 1)
+	{
+		if (maps[currSceneID]->changeScene == 2)
+		{
+			maps[currSceneID]->Exit();
+			SetNextScene(2);
+			currSceneID = nextSceneID;
+			maps[currSceneID]->Init();
+		}
+	}
+
+	maps[currSceneID]->Update(dt);
+	maps[currSceneID]->Render();
+}
